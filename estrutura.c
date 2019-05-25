cf0c57317738a24e53f8e4e679f4396066ceaabd
Enrick <enrickgsilva@hotmail.com>
2019-05-25 02:34:29 -0300

    Finalizada a estrutura básica de teste

--- estrutura.c
+++ estrutura.c
@@ -2,9 +2,9 @@
 
 struct gol {
     char nome[30];
-    int cpf;
+    char cpf[14];
     int idade;
-    int telefone;
+    char telefone[14];
     int ticket;
 };
 
@@ -20,25 +20,32 @@ int main() {
         scanf("%s", passageiro[i].nome);
         printf("\n");
         printf("CPF do passageiro: ");
-        scanf("%d", passageiro[i].cpf);
+        scanf("%s", passageiro[i].cpf);
         printf("\n");
         printf("Idade do passageiro: ");
-        scanf("%d", passageiro[i].idade);
+        scanf("%d", &passageiro[i].idade);
         printf("\n");
         printf("Telefone do passageiro: ");
-        scanf("%d", passageiro[i].telefone);
+        scanf("%s", passageiro[i].telefone);
         printf("\n");
         printf("Ticket do passageiro: ");
-        scanf("%d", passageiro[i].ticket);
+        scanf("%d", &passageiro[i].ticket);
         printf("\n");
+        system("cls");
     }
 
     for(int i = 0; i < n; i++) {
         printf("Nome: %s\n", passageiro[i].nome);
         printf("CPF: %s\n", passageiro[i].cpf);
         printf("Idade: %d\n", passageiro[i].idade);
-        printf("Telefone: %d\n", passageiro[i].telefone);
+        printf("Telefone: %s\n", passageiro[i].telefone);
         printf("Ticket: %d\n", passageiro[i].ticket);
+        printf("Assento numero %d\n", i+1);
+        if(passageiro[i].ticket == 1) {
+            printf("Primeira Classe!\n");
+            printf("Boa viagem!\n");
+        }
+        printf("\n\n");
     }
     return 0;
 }
