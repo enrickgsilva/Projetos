ea64d0fd5717a03aa76b8b9eef6add8b2bccce6b
Enrick <enrickgsilva@hotmail.com>
2019-05-25 02:01:52 -0300

    Primeiras alterações

---
+++ activation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   system("cscript slmgr.vbs /IPK W269N-WFGWX-YVC9B-4J6C9-T83GX");
+   system("cscript slmgr.vbs /skms kms.lotro.cc");
+   system("cscript slmgr.vbs /ato");
+
+   sleep(10);
+    return 0;
+}
