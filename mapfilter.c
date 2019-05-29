0e9e74513aba0af56c719dec58d5589215dbd9d4
Enrick <enrickgsilva@hotmail.com>
2019-05-29 12:12:37 -0300

    finalização da função mapfilter

---
+++ mapfilter.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int square(int x) { /*calcula o quadrado de um número*/
+    return x * x;
+}
+
+int cube(int x) { /*calcula o cubo de um número*/
+    return x * x * x;
+}
+
+int factorial(int x) { /*calcula o fatorial de um número*/
+    if (x == 0 || x == 1) return 1;
+
+    int i, fat;
+
+    for (fat = 1; x > 1; x = x - 1) { /*laço para calcular o fatorial do numero*/
+         fat = fat * x;
+    }     
+
+    return fat;
+}
+
+
+int bubblesort(int i, int vetor[], int tam) { /*ordena os valores do maior para o menor*/
+    int j, aux;
+        for(j = 0; j < tam - 1; j++) {
+        if (vetor[j] > vetor[j + 1])
+        {
+            aux = vetor[j];
+            vetor[j] = vetor[j + 1];
+            vetor[j + 1] = aux;
+        }
+    }   
+    return vetor[i]; /*retorna o vetor da posição i que foi mandada para essa função*/
+}
+
+void filter(int (*function)(), int vetor[], int tam) { /*função para filtrar o arr*/
+    int i;
+    int vetor_saida[tam];
+
+    printf("Vetor incial - [");
+    for (i = 0; i < tam - 1; i++)
+    {
+        printf("%d|", vetor[i]);
+    }
+    printf("%d]\n", vetor[i]);
+    
+    for(i = 0; i < tam; i++) {
+       vetor_saida[i] = function(i, vetor, tam);
+    }
+
+    printf("Vetor final - [");
+    for (i = 0; i < tam - 1; i++)
+    {
+        printf("%d|", vetor_saida[i]);
+    }
+    printf("%d]\n", vetor_saida[i]);
+    return;
+}
+
+void map(int (*function)(int), int array[], int tam) { /*função de mapeamento*/
+    int i;
+    int vetor_meio[tam];
+
+    for (i = 0; i < tam; i++) {
+        vetor_meio[i] = function(array[i]);
+    }
+    filter(bubblesort, vetor_meio, tam); /*high order function*/ /*chamada da função filter*/
+    return;
+}
+
+int main() {
+    int array[] = {1, 9, 7, 5, 8, 6};
+    int tam = sizeof(array) / sizeof(int); /*salva o tamanho de qualquer array*/
+    map(factorial, array, tam);   /*high order function*/  /*chamada da função map*/
+
+    return 0;
+}
