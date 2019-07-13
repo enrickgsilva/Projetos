dafce012531bed0377f2b4f44c28d80ca53355c2
enrickgsilva <44246751+enrickgsilva@users.noreply.github.com>
2019-07-13 12:04:16 -0300

    Add files via upload

---
+++ neo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  printf("Ola senhores, estão gostando de como nossas negociações estão se desenvolvendo?\n\n");
+  printf("Estou muito ansioso esperando por voces, espero que dessa vez seja tudo mais divertido pra todos!\n");
+  printf("Dessa vez estou deixando algo de valioso no caminho pra voces, porem dessa vez nao sei se todos vao alcancar seus objetivos!\n");
+  printf("Alguns estao bem aborrecidos com o que fiz, mas eu so mostrei que a populacao e facilmente manipulada\n");
+  printf("e que talvez voces estejam de um lado que nao faz tanto sentido....\n");
+  printf("quem sabe se na verdade as faccoes e associacoes so tem a opiniao popular baseada em mentiras como eu acabei de  fazer!\n");
+  Sleep(20000);
+  system("cls");
+  printf("Enfim eu so queria dizer que a morte de qualquer um, deveria ser crime nao e?.. mas pq voces acham ter carta branca para matar?\n");
+  return 0;
+}
